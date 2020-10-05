6302f37e6a0ea89237fb37d4f1a495b47422480c
Anton <oxlamons@gmail.com>
2020-10-05 21:21:05 +0300

    добавлено allow_overwrite

--- main.tf
+++ main.tf
@@ -2,6 +2,13 @@ provider "hcloud" {
   token = var.hcloud_token
   #base_url = "https://api.hetzner.cloud/v1/"
 }
+resource "random_password" "password" {
+  count   = "${length(var.domains)}"
+  length  = "10"
+  special = false
+  lower   = true
+  number  = true
+}
 data "hcloud_ssh_key" "rebrain_ssh_key" {
   name = "REBRAIN.SSH.PUB.KEY"
 }
@@ -15,37 +22,33 @@ resource "hcloud_ssh_key" "anton" {
   }
 }
 # Генерируется пароль
-resource "random_string" "password" {
-  count   = "${length(var.domains)}"
-  length  = "10"
-  special = true
-  lower   = true
-  number  = true
-}
+
 # Создаем VPC
-resource "hcloud_server" "node1" {
-  name        = "${element(var.domains, count.index)}.oxlamons.devops.rebrain.srwx.net"
+resource "hcloud_server" "renode1" {
   count       = "${length(var.domains)}"
+  name        = "${element(var.domains, count.index)}.oxlamons.devops.rebrain.srwx.net"
   image       = "ubuntu-18.04"
   server_type = "cx11"
   ssh_keys = [hcloud_ssh_key.anton.id,
   data.hcloud_ssh_key.rebrain_ssh_key.name]
 
+
   #Меняем пароль root на указаный в переменных и подключаюсь к VPC
 
   provisioner "remote-exec" {
     inline = [
-      "/bin/echo -e \"${element(random_string.password.*.result, count.index)}\n ${element(random_string.password.*.result, count.index)}\" | /usr/bin/passwd root"
+      "echo root:${random_password.password[count.index].result} | chpasswd"
     ]
     connection {
       type        = "ssh"
       user        = "root"
       private_key = file("~/.ssh/id_rsa")
       host        = self.ipv4_address
+
     }
   }
   provisioner "local-exec" {
-    command = "echo ${element(var.domains, count.index)}.oxlamons.devops.srwx.net root ${element(random_string.password.*.result, count.index)} >> login.txt"
+    command = "echo ${element(var.domains, count.index)}.oxlamons.devops.srwx.net root ${element(random_password.password.*.result, count.index)} >> login.txt"
 
   }
   labels = {
@@ -53,7 +56,6 @@ resource "hcloud_server" "node1" {
     "email" : "oxlamons_at_gmail_com"
   }
 }
-
 # Create a new provider using the SSH key
 provider "aws" {
   access_key = var.access_key
@@ -65,22 +67,23 @@ data "aws_route53_zone" "selected" {
   name = "devops.rebrain.srwx.net"
 }
 resource "aws_route53_record" "www" {
-  count   = "${length(var.domains)}"
-  zone_id = data.aws_route53_zone.selected.zone_id
-  name    = "oxlamons.${data.aws_route53_zone.selected.name}"
-  type    = "A"
-  ttl     = "300"
-  records = ["${element(hcloud_server.node1.*.ipv4_address, count.index)}"]
+  count           = "${length(var.domains)}"
+  zone_id         = data.aws_route53_zone.selected.zone_id
+  name            = "oxlamons.${data.aws_route53_zone.selected.name}"
+  type            = "A"
+  ttl             = "300"
+  records         = ["${element(hcloud_server.renode1.*.ipv4_address, count.index)}"]
+  allow_overwrite = true
 }
 # Вывод данных в консоль
-output "server_ip_node1" {
-  value = hcloud_server.node1.*.ipv4_address
+output "server_ip_renode1" {
+  value = hcloud_server.renode1.*.ipv4_address
 }
 
-output "server_id_node1" {
-  value       = hcloud_server.node1.*.id
+output "server_id_renode1" {
+  value       = hcloud_server.renode1.*.id
   description = "ID"
 }
-output "sever_password_node1" {
-  value = random_string.password.*.result
+output "sever_password_renode1" {
+  value = random_password.password.*.result
 }
